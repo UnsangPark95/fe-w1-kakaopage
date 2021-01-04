317a4c3f1010ffe41422fd0dea79fadf76e38f06
Kevin home <kevin.home@kakaocorp.com>
2021-01-04 19:42:21 +0900

    Header, navbar

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'))
 
 app.listen(port, () => {
-  console.log("My first project in kakao:)");
+  console.log('Conneted', port, 'port!');
 });
